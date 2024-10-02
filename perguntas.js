88488fd8e0f5703b827776a73fba0947b3cd4a7c
CauanEgidio <142343859+CauanEgidio@users.noreply.github.com>
2024-10-02 19:17:08 +0000

    cartao

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Porgramação',
+    'O que é uma Função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
